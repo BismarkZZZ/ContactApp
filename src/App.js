998028a011cac241e9263806a41266889d69131b
Tardieh <tardiehbismark@hotmail.com>
2021-03-21 23:03:26 -0400

    comment out Add New Contaact function

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ import AddContact from './component/AddContact';
 
 class App extends Component {
   
-  addNewContact = (contacts) => {
-    contacts.id = Math.random().toString();
-    this.setState({
-      contacts: [...this.state.contacts, contacts]
-    })
-  }
+  // addNewContact = (contacts) => {
+  //   contacts.id = Math.random().toString();
+  //   this.setState({
+  //     contacts: [...this.state.contacts, contacts]
+  //   })
+  // }
+
   deleteContact = (id) => {
     let undeletedContacts = this.state.contacts.filter(contacts => contacts.id !== id)
     this.setState({
       contacts: undeletedContacts
     })
   }
+
   editContact = (id, updatedContact) => {
     this.setState({
       contacts: this.state.contacts.map(contacts => contacts.id === id ? updatedContact : contacts)
     })
   }
+  
   render() {
     return (
       <Container fluid style={{ marginTop: '2rem' }}>
         <Row>
           <Col md='4'>
             <h3>New Contact</h3>
-            <AddContact addContact={this.addNewContact} />
+            <AddContact addContact={this.addContact}/>
           </Col>
           <Col>
             <h3>Contact Page</h3>

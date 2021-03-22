1ce477b76e3e27ee2a30a16727213ccdb86a11be
Tardieh <tardiehbismark@hotmail.com>
2021-03-22 06:13:20 -0400

    Add firestore and firebase to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,21 @@
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, compose, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
 import contactReducer from "../reducers/contactsReducer";
 import authReducer from "../reducers/authReducer";
+import {getFirebase, reactReduxFirebase} from "react-redux-firebase";
+import { getFirestore, reduxFirestore} from "redux-firestore";
+import firebase from "../firebase/firebase";
+
+const reducers = combineReducers ({
+    contactsState: contactReducer,
+    auth: authReducer,
+})
 
 export const store = createStore (
-    combineReducers ({
-        contactsState: contactReducer,
-        auth: authReducer,
-    })
+    reducers,
+    compose (
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+        reactReduxFirebase(firebase),
+        reduxFirestore(firebase),
+    )
 );
